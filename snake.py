9c40c10ec94c6bd9703444b2dfc1aead5579fb41
Brian <briancdang@gmail.com>
2020-11-21 04:52:33 +0000

    Avoid long snake head collision better

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 import random
 
-from board import Board, TokenType
+from board import Board, TokenType, Weight
 from enum import Enum
 from a_star import a_star
 
@@ -28,26 +28,6 @@ class Snake:
     self.my_head = me["head"]
     self.my_tail = me["body"][-1]
     self.my_health = me["health"]
-
-  # Reduce possible_moves to avoid death from walls, snakes, or hazards
-  def avoid_death(self):
-    (x, y) = self.my_head
-
-    new_moves = [Move.UP, Move.DOWN, Move.LEFT, Move.RIGHT]
-
-    above = {"coord": (x, y + 1), "move": Move.UP}
-    below = {"coord": (x, y - 1), "move": Move.DOWN}
-    left = {"coord": (x - 1, y), "move": Move.LEFT}
-    right = {"coord": (x + 1, y), "move": Move.RIGHT}
-
-    coords_to_check = [above, below, left, right]
-
-    for coord in coords_to_check:
-      token = self.board.token(coord["coord"])
-      if token == TokenType.OUT_OF_BOUNDS or token == TokenType.HAZARD or token == TokenType.SNAKE:
-        new_moves.remove(coord["move"])
-        
-    return new_moves
 
   # Get the move direction
   # This assumes the arguments are neighbors.
@@ -78,24 +58,24 @@ class Snake:
     # 2. If unhealthy, pick direction based on best path to food
     nearest_food = self.board.nearest_food(self.my_head)
     if nearest_food != None:
-      food_path = a_star(self.board, self.my_head, nearest_food)
-      if len(food_path) > 1 :
-        return self.direction(food_path[0], food_path[1])
+      direction = self.direction_to_coord(nearest_food)
+      if direction != None:
+        return direction
 
     # If no path to food, default to tail path
-    direction = self.direction_to_tail()
-    if direction != None:
-      return direction
+    if self.my_health <= 20:
+      direction = self.direction_to_tail()
+      if direction != None:
+        return direction
 
-    # If no tail path, then...
-
-    # TODO: Add more logic as we learn it
-
-    # Default choose random. Points to area for improvement.
-    print("Failed to path. Defaulting to random.")
-    possible_moves = self.avoid_death()
-    if len(possible_moves) > 0:
-      return random.choice(possible_moves)
+    # Fallback: choose lowest weight neighbor
+    safe_neighbors = self.board.safe_neighbors(self.my_head)
+    if len(safe_neighbors) > 0:
+      weighted_neighbors = []
+      for neighbor in safe_neighbors:
+        weighted_neighbors.append((self.board.get_weight(neighbor), neighbor))
+      weighted_neighbors.sort()
+      return self.direction(self.my_head, weighted_neighbors[0][1])
     else:
       print("Trapped! Defaulting to up.")
       return Move.UP
@@ -107,10 +87,22 @@ class Snake:
       # Loop through all neighbors in case the first neighbor is unpathable
       for tail_neighbor in tail_neighbors:
         # TODO: Predict future and see if we will trap ourselves if we go to this square, e.g. if we just ate food
-        tail_path = a_star(self.board, self.my_head, tail_neighbor)
-        if len(tail_path) > 1 :
-          return self.direction(tail_path[0], tail_path[1])
+        direction = self.direction_to_coord(tail_neighbor)
+        if direction != None:
+          return direction
     
     # TODO: Chase snake tail instead of neighbor, especially if we know it will move.
 
     return None
+
+  # A* search to coord
+  def direction_to_coord(self, coord):
+    path = a_star(self.board, self.my_head, coord)
+    if len(path) > 1 and self.is_low_risk(path[1]):
+      return self.direction(path[0], path[1])
+    else:
+      return None
+
+  # Return true if risk of moving to coord is low
+  def is_low_risk(self, coord):
+    return self.board.get_weight(coord) < Weight.LONG_SNAKE_HEAD.value
--- test_snake.py
+++ test_snake.py
@@ -20,73 +20,7 @@ class TestSnake(unittest.TestCase):
       }
     }
 
-  def test_avoid_wall(self):
-    test_data = self.default_data
-    merge_dict(test_data, {
-      "board": {
-        "width": 10,
-        "height": 10
-      },
-      "you": {
-        "head": (0, 9)
-      }
-    })
-    snake = Snake(test_data)
-    new_moves = snake.avoid_death()
-    self.assertEqual(new_moves, [Move.DOWN, Move.RIGHT])
-
-  def test_avoid_hazard(self):
-    test_data = self.default_data
-    merge_dict(test_data, {
-      "board": {
-        "hazards": [(0, 1)]
-      },
-      "you": {
-        "head": (1, 1)
-      }
-    })
-    print(test_data)
-    snake = Snake(test_data)
-    new_moves = snake.avoid_death()
-    self.assertEqual(new_moves, [Move.UP, Move.DOWN, Move.RIGHT])
-
-  def test_avoid_own_body(self):
-    test_data = self.default_data
-    merge_dict(test_data, {
-      "board": {
-        "snakes": [{
-          "id": "me",
-          "head": (1, 1),
-          "body": [(1, 1), (1, 0)]
-        }]
-      },
-      "you": {
-        "head": (1, 1),
-        "body": [(1, 1), (1, 0)]
-      }
-    })
-    snake = Snake(test_data)
-    new_moves = snake.avoid_death()
-    self.assertEqual(new_moves, [Move.UP, Move.LEFT, Move.RIGHT])
-
-  def test_avoid_other_snake(self):
-    test_data = self.default_data
-    merge_dict(test_data, {
-      "board": {
-        "snakes": [{
-          "id": "them",
-          "body": [(2, 1)]
-        }]
-      },
-      "you": {
-        "head": (1, 1)
-      }
-    })
-    snake = Snake(test_data)
-    new_moves = snake.avoid_death()
-    self.assertEqual(new_moves, [Move.UP, Move.DOWN, Move.LEFT])
-
-  # Unit test built off of loss:
+  # Test built off of loss:
   # https://play.battlesnake.com/g/df436e2e-4dba-4fac-abbf-e7736aeef3cc/
   def test_take_safer_path(self):
     test_data = self.default_data
@@ -124,7 +58,7 @@ class TestSnake(unittest.TestCase):
 
     self.assertEqual(snake.next_move(), "right")
 
-  # Unit test built off of loss:
+  # Test built off of loss:
   # https://play.battlesnake.com/g/858d116f-adb5-4c3c-acdf-63dfbed216c5/
   def test_head_to_head_worse_than_hazards(self):
     test_data = self.default_data
@@ -164,6 +98,44 @@ class TestSnake(unittest.TestCase):
     """
 
     self.assertEqual(snake.next_move(), "down")
+
+  # Test built off of loss:
+  # https://play.battlesnake.com/g/5a9c924a-6bd3-460e-8e40-5c39a757fb44/
+  def test_avoid_pathing_to_long_snake_head(self):
+    test_data = self.default_data
+    my_snake = {
+      "id": "me",
+      "head": (2, 3),
+      "body": [(2, 3), (2, 4), (1, 4)],
+      "health": 100
+    }
+    merge_dict(test_data, {
+      "board": {
+        "width": 5,
+        "height": 5,
+        "snakes": [
+          my_snake,
+          {
+            "id": "them",
+            "head": (0, 3),
+            "body": [(0, 3), (0, 2), (1, 2), (1, 1), (1, 0)],
+            "health": 100
+          }
+        ]
+      },
+      "you": my_snake
+    })
+    snake = Snake(test_data)
+
+    """
+    | |ø|ø| | |
+    |^| |v| | |
+    |■|■| | | |
+    | |■| | | |
+    | |■| | | |
+    """
+
+    self.assertNotEqual(snake.next_move(), "left")
 
 if __name__ == '__main__':
   unittest.main()
